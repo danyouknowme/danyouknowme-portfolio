50f3011ef49c8d7617575abfa8516e8579193a62
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-07-22 00:04:03 +0700

    building a dynamically nav bar

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,8 @@
-import "testimonials.scss";
+import "./testimonials.scss";
 
 export default function Testimonials() {
     return (
-        <div className="testimonials">
+        <div className="testimonials" id="testimonials">
             
         </div>
     )
