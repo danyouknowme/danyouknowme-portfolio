9b856a1814287e13689a06377a9dec5084bf13f8
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-07-28 17:19:56 +0700

    change name of logo, add box-shadow of nav

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
         <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">genuis.</a>
+                    <a href="#intro" className="logo">Thanathip S.</a>
                     <div className="itemContainer">
                         <Phonelink className="icon" />
                         <span>095-168-3442</span>
